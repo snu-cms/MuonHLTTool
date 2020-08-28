255b0ec4157bb9f02b46909bfb2d2a6e62020ad4
Minseok Oh <minseok.oh@cern.ch>
2020-08-28 15:22:42 +0900

    minor

--- MuonHLTNtupler/interface/MuonHLTSeedNtupler.h
+++ MuonHLTNtupler/interface/MuonHLTSeedNtupler.h
@@ -75,7 +75,7 @@
 #include "Geometry/CommonDetUnit/interface/GeomDetType.h"
 #include "Geometry/CommonDetUnit/interface/GeomDet.h"
 
-#include "// Geometry/CommonTopologies/interface/PixelGeomDetUnit.h"
+// #include "Geometry/CommonTopologies/interface/PixelGeomDetUnit.h"
 #include "Geometry/CommonTopologies/interface/PixelGeomDetType.h"
 #include "Geometry/TrackerGeometryBuilder/interface/PixelTopologyBuilder.h"
 #include "Geometry/Records/interface/StackedTrackerGeometryRecord.h"
